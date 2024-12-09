37036f810e2d3bd7e1c4144e83ded186ea80bada
Hugo Duque <hugoduque@Hugos-MacBook-Pro.local>
2024-12-08 21:07:20 -0400

    final product

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <main className="">
+          {children}
+        </main>
       </body>
     </html>
   );
